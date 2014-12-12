d19aad0252502cb26e5933e22060ea82a7d1c5eb
Dong Qingshan <dongqs@gmail.com>
2014-12-12 11:26:50 +0800

    improve help

--- lib/crosstie/cli.rb
+++ lib/crosstie/cli.rb
@@ -5,26 +5,18 @@ module Crosstie
 
   class CLI < Thor
 
-    default_task :help
-
-    desc "new", "create a new rails application"
+    desc "new my_app", "create a new rails application"
     def new name
       cmd = "rails new #{name} --template #{template_path}"
       puts cmd
       system cmd
     end
 
-    desc "version", "print the version of current crosstie"
+    desc "version", "print current version"
     def version
       puts "crosstie #{Crosstie::VERSION}"
     end
     map %w(-v --version) => :version
-
-    desc "help", "print the help"
-    def help
-      puts "USAGE:\n\tcrosstie new my_app"
-    end
-    map %w(-h --help) => :help
 
     private
 
